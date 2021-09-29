c70715c2cda7070b44ca2f96f608cd787ac82aff
lostinthestack <lostinthestack@protonmail.com>
2021-09-29 18:45:18 -0500

    Initial commit

---
+++ MethodsExercise02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodsExercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int x = 9, y = 3;
+
+        Console.WriteLine(Add(x, y));
+        Console.WriteLine(Subtract(x, y));
+        Console.WriteLine(Multiply(x, y));
+        Console.WriteLine(Divide(x, y));
+        Console.WriteLine(Modulus(x, y));
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+    }
+}
